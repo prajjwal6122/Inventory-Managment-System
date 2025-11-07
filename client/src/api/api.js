a082f5b641d57747596f842503802341ed54494a
prajjwalarizon <prajjwal6122@gmail.com>
2025-11-07 19:35:01 +0530

    Build for front end

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,16 @@
-import axios from 'axios';
-const API = axios.create({ baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000' });
-API.interceptors.request.use(req => {
-  const token = localStorage.getItem('token');
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://fifo-inventory-managment-system.onrender.com"
+      : "http://localhost:3000",
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
+
 export default API;
